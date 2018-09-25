0b1e45287f9cd2d175d05d59d1a10e3a079d7e2b
maxleon52 <maxleon522@gmail.com>
2018-09-25 11:47:07 -0300

    ATUALIZAÇÕES ATRASADAS

--- consulta_clientes_app/views.py
+++ consulta_clientes_app/views.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
 from cad_clientes_app.forms import clienteForm
 from cad_clientes_app.models import tb_clientes
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import get_template
+
 
 
 # CONSULTA CLIENTES
@@ -12,11 +15,13 @@ def lista_clientes(request):
     cpf = request.GET.get('cpf', None)
 
     if razNome or cnpj or cpf:
-        lista = tb_clientes.objects.filter(nome__icontains=razNome, cnpj__icontains=cnpj, cpf__icontains=cpf)  # CONSULTA NO MODEL (BD) E ARMAZENA NA VARIAVEL
+        lista = tb_clientes.objects.filter(nome__icontains=razNome, cnpj__icontains=cnpj,
+                                           cpf__icontains=cpf)  # CONSULTA NO MODEL (BD) E ARMAZENA NA VARIAVEL
     else:
         lista = tb_clientes.objects.all()  # CONSULTA NO MODEL (BD) E ARMAZENA NA VARIAVEL
 
-    return render(request, 'consulta_clientes_app/consultaClientes.html', {'lista': lista})  # MOSTRANDO O TEMPLATE E A CONSULTA NO BANCO
+    return render(request, 'consulta_clientes_app/consultaClientes.html',
+                  {'lista': lista})  # MOSTRANDO O TEMPLATE E A CONSULTA NO BANCO
 
 
 # METODO QUE REDIRECIONA A PAGINA e GERA O FORM NA TELA
@@ -57,3 +62,13 @@ def deleteCliente(request, id):
         return redirect('lista_clientes')
 
     return render(request, 'consulta_clientes_app/confDeleteCliente.html', {'form': form})
+
+
+# REPORTLAB
+# TESTE DE GERAÇÃO DE PDF---GERA MAIS NÃO ABRE
+def pdf_generation(request):
+    html_template = get_template('consulta_cliente_app/helloPdf.html')
+    pdf_file = HTML(string=html_template).write_pdf()
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="hello.pdf"'
+    return response
